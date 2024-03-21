1f94d8522ae9be85f49e6e68f8c9d0bf7512b6b5
Terentev Roman <saradventure@andex.ru>
2024-03-22 01:26:19 +0400

    добавил файл Program.cs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("Введите несколько слов в одну строчку");
+string s = Console.ReadLine();
+string[] stringArray = s.Split(' ');
+for(int i = 0; i< stringArray.Length; i++)
+{
+    if(stringArray[i].Length<=3)
+    {
+        Console.Write(stringArray[i] + " ");
+    }
+}
